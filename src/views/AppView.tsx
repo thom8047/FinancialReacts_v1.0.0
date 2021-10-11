fae1262cc23bcd7cd13819e4c8e87d9552593c64
thom8047 <thom8047@bears.unco.edu>
2021-10-10 22:31:04 -0600

    Updated the gitignore to ignore the python modules folder. Updated some css and some state managment such as name and data. BIG UPDATES: Got parse.ts to organize data by date and return that as the data. This is still not fill proof and no testing has been done to see how it measures up. The python script also captures all dates if that was not previously mentioned. I can still get totals, and now I can organize the data acccordingly and filter by what months should/should not be there ie. From Aug 21-Oct 21, and this will change the state of the data so we can look at specific instances.

--- src/views/AppView.tsx
+++ src/views/AppView.tsx
@@ -4,33 +4,70 @@ import parseData from "../algorithm/parse";
 import Chart from "./Chart";
 import React from "react";
 
-const _data = parseData();
+let parsedData: any = parseData();
+let allData: any[] = [];
+
+Object.entries(parsedData).forEach(([k, v]) => {
+  allData = allData.concat(parsedData[k].transaction);
+});
 
 function AppView() {
-  const [data, setData] = React.useState(_data.Statement0.transaction);
-  const name: string = "Camryn";
+  // States
+  const [data, setData] = React.useState(allData);
+  const [name, setName] = React.useState("Camryn");
+
+  // Const variables that are based on data to be displayed
+  const totalExp = () => {
+    let sum: number = 0;
+    for (let trans of data) {
+      sum += parseFloat(trans.CHARGE);
+    }
+    return sum.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+  };
+  const returnBold = (inner: string | number) => (
+    <b className="report-bold">{inner}</b>
+  );
+
+  // Comst event handlers
   const handleEvent = (event: any) => {
-    setData(data.concat(_data.Statement1.transaction));
-    // console.log(cleanedData.Statement0.transaction);
+    setName(() => (name === "Camryn" ? "Kyle" : "Camryn"));
   };
 
   return (
     <div>
       <div className="App-header">
         <h1>WELCOME {name.toUpperCase()}!</h1>
+        <button className="change-user-btn" onClick={handleEvent}>
+          CHANGE USER
+        </button>
       </div>
       {/* This will be the bar under the app header to display from to and anything else */}
-      <div>
-        <button onClick={handleEvent}>TEST</button>
+      <div className="navbar">
+        <span>FROM: </span>
+        <span>TO: </span>
       </div>
-      {/* MOVE CSS TO STYLES SHEET */}
-      <div style={{ display: "flex" }}>
-        <div style={{ flex: 1 }}>
+      <div className="App-chart-n-rep-parent">
+        <div className="chart-n-rep-child">
           <ResponsiveContainer width="50%" height="100%">
             <Chart data={data} />
           </ResponsiveContainer>
         </div>
-        <div style={{ flex: 1 }}>Hello</div>
+        <div className="chart-n-rep-child">
+          <div className="report">
+            <div className="report-child">
+              Overall monthly expenses:
+              {returnBold("$" + totalExp())}
+            </div>
+            <div className="report-child">
+              Increase in spending from prev month by __%
+            </div>
+            <div className="report-child">Largest expense of the month: </div>
+            <div className="report-child">Savings: </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/views/Chart.tsx
+++ src/views/Chart.tsx
@@ -10,6 +10,20 @@ import {
 import React from "react";
 
 function Chart(props: any) {
+  const getLargestPurchase = (): number => {
+    let max: number = 0;
+    for (let trans of props.data) {
+      if (parseFloat(trans.CHARGE) > max) {
+        max = parseFloat(trans.CHARGE);
+      }
+    }
+    return max;
+  };
+
+  const handleClick = () => {
+    console.log("test");
+  };
+
   return (
     <LineChart
       width={500}
@@ -19,10 +33,15 @@ function Chart(props: any) {
     >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="TRANS_DATE" />
-      <YAxis domain={[0, 130]} />
+      <YAxis domain={[0, getLargestPurchase()]} />
       <Tooltip />
       <Legend />
-      <Line type="monotone" dataKey="CHARGE" stroke="#FF7F7F" />
+      <Line
+        type="monotone"
+        dataKey="CHARGE"
+        stroke="#FF7F7F"
+        onMouseDown={handleClick}
+      />
       {/*#8884d8*/}
     </LineChart>
   );
